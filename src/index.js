9b4fa3832a0c29181322f42e4dbed141d985b2c5
Soo <parksk971031@gmail.com>
2023-04-11 17:41:36 -0400

    submit sa2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('starting up!');
+// import $ from 'jquery';
+// const $ = require('jquery');
+import './style.scss';
+
+// $('#main').html('Here we go!');
+
+let cnt = 0;
+const main = document.querySelector('#main');
+
+setInterval(() => {
+  cnt += 1;
+  main.textContent = `You've been on this page for ${cnt} seconds.`;
+}, 1000);
