4637234ef62fd957709253acbe06a9c5f5413fef
Soo <parksk971031@gmail.com>
2023-04-11 18:29:59 -0400

    add favicon otw

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,15 @@ import eslint from 'vite-plugin-eslint';
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 
+import favicon from 'vite-plugin-favicon';
+
 import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [eslint()],
+  plugins: [eslint(), favicon({
+    src: './img/favicon',
+  })],
   css: {
     postcss: {
       plugins: [
